4ff05777ed4020ca7b86bb4f613588d2738a2564
liudehuang <2969878315@qq.com>
2021-02-28 17:49:22 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!--头像区域-->
+      <div class="avatar_box">
+        <img src="../assets/logo.png">
+      </div>
+      <!--表单区域-->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!--账号区域-->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-usercenter" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!--密码区域-->
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-password" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login.vue',
+  data () {
+    return {
+      /* 登录表单数据绑定对象 */
+      loginForm: {
+        username: 'wangwu',
+        password: '123456'
+      },
+      /*  表单的规则验证对象 */
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 8, message: '长度在 5 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 5, max: 8, message: '长度在 5 到 8 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      console.log(this)
+      // eslint-disable-next-line no-unused-expressions
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        if (!valid) {
+          return;
+        }
+        // const { data: res } = await this.$http.post('login', this.loginForm);
+        this.$message.error("登录失败")
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar_box{
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  img{
+    height: 100%;
+    width: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = "http://127.0.0.1:8080/test"
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
